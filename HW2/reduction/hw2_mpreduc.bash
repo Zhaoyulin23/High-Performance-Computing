2ec2e86197318a832902913371e5461fe9a86816
Zhaoyulin23 <zhaoyulin1989@gmail.com>
2017-12-01 18:26:16 +0800

    Parallel and Sequential

--- HW2/reduction/hw2_mpreduc.bash
+++ HW2/reduction/hw2_mpreduc.bash
@@ -1,11 +1,11 @@
 #!/bin/sh
-#BSUB -J tancheng-hw2
+#BSUB -J yulin-hw2
 #BSUB -o output_file_reduc30
 #BSUB -e error_file_reduc30
 #BSUB -n 10
 #BSUB -q ht-10g
-#BSUB cwd /home/zhuang.t/HW2/
-work=/home/zhuang.t/HW2
+#BSUB cwd /home/zhao.y/HW2/
+work=/home/zhao.y/HW2
 
 cd $work
 export OMP_NUM_THREADS=30
--- HW2/sequential/hw2-sequential.bash
+++ HW2/sequential/hw2-sequential.bash
@@ -1,11 +1,11 @@
 #!/bin/sh
-#BSUB -J tancheng-hw2
+#BSUB -J yulin-hw2
 #BSUB -o output_file_se
 #BSUB -e error_file_se
 #BSUB -n 10
 #BSUB -q ht-10g
-#BSUB cwd /home/zhuang.t/HW2/
-work=/home/zhuang.t/HW2
+#BSUB cwd /home/zhao.y/HW2/
+work=/home/zhao.y/HW2
 
 cd $work
 export OMP_NUM_THREADS=20
